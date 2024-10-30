4ca637887750a88ebfcb8557499b7cb34fcdcb62
Renz Jay <renzjaybayeng@gmail.com>
2024-10-30 11:27:18 +0800

    asdsa

--- self-paced-activity/delete.php
+++ self-paced-activity/delete.php
@@ -4,7 +4,7 @@ include 'db.php';
 
 
 if (isset($_GET['id'])) {
-    $id = $_GET['id'];
+    $id = (int)$_GET['id'];
 
     $stmt = $conn->prepare("DELETE FROM users WHERE id = ?");
     $stmt->bind_param("i", $id);
