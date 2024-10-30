ef72d86bb28cf95fe509521e2531d00bcf9aa87f
Renz Jay <renzjaybayeng@gmail.com>
2024-10-30 11:44:54 +0800

    sadwds

--- self-paced-activity/index.php
+++ self-paced-activity/index.php
@@ -1,51 +1,50 @@
-    <?php
+<?php
 
-    include "db.php";
+include "db.php";
 
-    $sql = "SELECT * from users";
-    $result = $conn->query($sql);
+$sql = "SELECT * from users";
+$result = $conn->query($sql);
 
-    ?>
+?>
 
+<!DOCTYPE html>
+<html lang="en">
 
-    <!DOCTYPE html>
-    <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Users Table</title>
+</head>
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Users Table</title>
-    </head>
+<body>
+    <h2>List of Users</h2>
 
-    <body>
-        <h2>List of Users</h2>
+    <table border="1">
+        <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Delete</th>
+            <th>Update</th>
+        </tr>
 
-        <table border="1">
-            <tr>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Action</th>
-                <th>Action</th>
-            </tr>
+        <?php
 
-            <?php
-
-            if ($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-                    echo "<tr>";
-                    echo "<td>" . $row['name'] . '</td>';
-                    echo "<td>" . $row['email'] . '</td>';
-                    echo '<td><a href="delete.php?id="' . $row['id'] . '>Delete</a></td>';
-                    echo '<td><a href="update.php?id="' . $row['id'] . '>Update</a></td>';
-                }
-            } else {
-                echo "<tr><td colspan='3'>No Contacts</td></tr> ";
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>" . $row['name'] . "</td>";
+                echo "<td>" . $row['email'] . "</td>";
+                echo '<td><a href="delete.php?id=' . $row['id'] . '">Delete</a></td>';
+                echo '<td><a href="update.php?id=' . $row['id'] . '">Update</a></td>';
+                echo "</tr>";
             }
+        } else {
+            echo "<tr><td colspan='4'>No Contacts</td></tr>";
+        }
 
-            ?>
-        </table>
+        ?>
+    </table>
 
+</body>
 
-    </body>
-
-    </html>
+</html>
